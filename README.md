fa012b73c71db4cb0b2e106515814b3fd44d6a66
EricLu <eric.lu@linecorp.com>
2024-06-21 08:38:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_line_sdk_mysample
+
+https://github.com/line/flutter_line_sdk/issues/101
---
+++ android/app/src/main/kotlin/com/linesdksample/flutter_line_sdk_mysample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.linesdksample.flutter_line_sdk_mysample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
